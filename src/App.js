57bd83ea3c0f3602ffc17f7d3e0e0d5fb9c555c1
Raman <ramanmcreates@gmail.com>
2023-04-28 14:20:32 -0400

    media

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import SubmissionCard from './SubmissionCard';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
+
+
+
+<div className="App">
       <SubmissionCard />
     </div>
+
+
+    
+
   );
 }
 
