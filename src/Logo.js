e3c5ce954d68127a4038ca052d18fc8846a18980
Raman <ramanmcreates@gmail.com>
2023-05-10 15:24:16 -0400

    image

--- src/Logo.js
+++ src/Logo.js
@@ -1,10 +1,8 @@
-import logo from "/Users/Raman/Desktop/Development/personal/songshare.io/src/songshare.io logo.001.jpeg"
 import newlogo from "/Users/Raman/Desktop/Development/personal/songshare.io/src/songshare new.001.jpeg"
 
 function Logo() {
     return (
       <div className="logocontainer">
-        {/* <h1>songshare.io BETA</h1> */}
         <img className="logo" src={newlogo}/>
       </div>
     );
